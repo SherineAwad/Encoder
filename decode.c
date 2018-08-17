4696c918616daf3d5e1def12bb8b2d7e0e700b49
sherineawad <sherine.awad@gmail.com>
2018-08-17 13:48:51 +0100

    update encode and decode

--- decode.c
+++ decode.c
@@ -14,15 +14,6 @@ printf("ROWS and COLS %d,%d \n", ROWS, COLS);
 for(i=0;i<ROWS;i++)
 	for(j=0;j<COLS;j++) 
 	     datat[i][j] = data[j][i];//{printf("%d, %d,%d \n", j, i, data[j][i]);datat[i][j]= data[j][i];}
-
-//----print 
-/*printf("Here we print from new transpose \n");
-for(i =0;i<ROWS; i++)
-{
-for(j=0;j<COLS;j++)
-        printf("%d",datat[i][j]);
-printf("\n");
-}*/
 return(datat);
 }
 //--------------------------------------Decode--------------------------------------
@@ -34,7 +25,7 @@ int i, j; //iterators
 printf("We are here in the decoding process \n");
 fp = fopen(file, "r");
 fscanf(fp, "%d,%d", &ROWS, &COLS); //first row in the encoded file is the rows and cols of the new encoded array.
-printf("after reading %d %d \n", ROWS, COLS);  
+printf("After reading %d %d \n", ROWS, COLS);  
 //declare your array of the sixe you read from header of encoded file 
 int **datat = malloc(ROWS * sizeof(int*));
 for (i = 0; i < ROWS; i++)
@@ -52,29 +43,18 @@ int deli_count =0;
 while(1)
 {
      if(deli_count >=ROWS) break;
-     //printf("Deli count is %d  \n", deli_count);
      j=0;
-     //printf("We are in the outer loop  \n");
      fscanf(fp,"%d,", &i); //first element in compressed file is the row id
      if (i ==deli) break;
      while(1)
                 {
-		//printf("We are in inner loop \n");
                 fscanf(fp,"%d,",&j);
-		//printf("i is and j is %d %d \n",i,j);
 		if(j==deli) {deli_count+=1;break; }
                 datat[i][j]=1;
                 }
 if(feof(fp) ) break;
 }
 fclose(fp);
-/*printf("We are here printing datat from inside decode \n"); 
-for(i =0;i<ROWS;i++)
-	{
-        for(j =0;j<COLS;j++) 
-		printf("%d,", datat[i][j]);
-	printf("--\n");
-        }*/
 *r =ROWS;
 *c =COLS;
 return(datat);
@@ -82,43 +62,27 @@ return(datat);
 //-------------------------------------Main Function----------------------------
 int main(int argc, char **argv)
 {
-if (argc < 2)
+if (argc < 3)
         {
-        printf("Incorrect no. of input, check you entered the row id you wish to return 0 is the first row, 1 is the second row, ..etc \n");
+        printf("Incorrect no. of input, enter file to decode  the row id you wish to return 0 is the first row, 1 is the second row, ..etc \n");
         return(1);
         }
-int rowid = atoi(argv[1]);
-char * infile = argv[2];
+int rowid = atoi(argv[2]);
+char * infile = argv[1];
 FILE *fp; 
 int i,j; 
 const int max= 1000000; 
 int  ROWS =max;
 int  COLS =max;
-int **data = malloc(max * sizeof(int*));
-for (i = 0; i <max; i++)
-    data[i] = malloc(max * sizeof(int));
-printf("ROWS and COLS %d %d \n", ROWS, COLS);
-fp = fopen(infile, "r+t");
-int** newd1 = decode("compressed.txt",&ROWS,&COLS); 
+int** datat = decode(infile,&ROWS,&COLS); 
 printf("We are done after decoding and R, C are : %d %d \n",ROWS, COLS);
-int** newd2 = transpose(newd1,COLS,ROWS); //swap COLS and ROWS for transpose 
+int** data = transpose(datat,COLS,ROWS); //swap COLS and ROWS for transpose 
 printf("%s", "We are done transposing to original form \n");
 printf("row id is %d \n", rowid);
-//rowid = rowid -1; // we start with zero  
 for (i =0; i<ROWS; i++) //we are back to the original size 
-	printf("%d %c", newd2[rowid][i],' '); 
-//printf("%s", '\n');
-/*for(i=0;i<COLS;i++) 
-  {
-   for(j=0;j<ROWS;j++)
-        printf("%d %c", newd2[i][j], ' ');
-   printf("%c",'\n');
-  }
-printf("%s", "Done printing what we need \n"); 
-*/
-//fclose(fp);
-//free(newd1);
-//free(newd2);
+	printf("%d %c", data[rowid][i],' '); 
+free(data);
+free(datat);
 return(0);
 }
 
--- encode.c
+++ encode.c
@@ -4,13 +4,10 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <zlib.h>
-#include "data.h"
 //-------------------------Encode Transpose----------
 int** transpose(int **data, int ROWS, int COLS )
 {
 int temp = ROWS;
-//ROWS =COLS;
-//COLS = temp;
 int i, j;
 printf("We will start transposing with passed ROWS $ COLS %d %d\n", ROWS, COLS);
 int **datat = (int **)malloc(COLS * sizeof(int *));
@@ -24,7 +21,7 @@ for(i=0;i<COLS;i++)
 return(datat);
 }
 //-------------------------------------------------------------------------------
-/*int ** read_file(char* infile, int *rows, int *cols)
+int ** read_file(char* infile, int *rows, int *cols)
 {
 FILE * fp;
 char * line = NULL;
@@ -42,49 +39,35 @@ for (i = 0; i < ROWS; i++)
 
 fp = fopen(infile, "r");
 if (fp == NULL)
-  {
-    printf("Error opening");
-    exit(EXIT_FAILURE);
-  }
-int myrow =0; 
+      {
+          printf("Error opening");
+          exit(EXIT_FAILURE);
+      }
+int myrow =0;
+j =-1;
 while ((read=getline(&line, &len, fp)) != -1)
   {
-     j =0; 
-     token = line;
-     token = strtok(line, sep);
-     printf("len is %d \n", sizeof(line));
-     printf("Line is %s \n", line);
-     while(token != NULL)
-     {
-       //printf("token is %s \n", token);
-       //printf("printing atoi %d , row is %d , cols is %d \n", atoi(line), myrow,j);
-       data[myrow][j] = atoi(token);
-       token=strtok(NULL,sep);
-       j+=1;
-     }
-  // printf("j is %d \n", j);
-   myrow +=1; 
-  }
+   j =-1;
+   token = strtok(line, sep);
+   while( (token != NULL) ) 
+   	{
+   	j+=1;
+   	data[myrow][j] = atoi(token);
+   	token=strtok(NULL,sep);
+   	}
+   myrow +=1;
+ }
 ROWS =myrow;
-COLS = j-1;
+COLS = j+1;
 *rows = ROWS;
 *cols = COLS;
-printf("Rows is %d and COLS is %d \n", ROWS, COLS);
-for(i=0;i<ROWS;i++) 
-{
-   for(j=0;j<COLS;j++) 
-       {printf("%d,", data[i][j]);}
-   printf("$");
-}
-
 return data;
-}i */
+}
 //-------------------------------------Compress each row-------------------------
 void compressf(int** datat, int ROWS, int COLS, char* outfile)
 {
 int i, j; //iterators 
 FILE *fp;
-//int deli =-1;
 int  deli =-1;
 fp = fopen(outfile, "w+t");
 if ( fp == NULL )
@@ -114,10 +97,11 @@ printf("Done Compressing \n");
 int main(int argc, char **argv)
 {
 char * infile = argv[1];
+char * outfile = argv[2];
 FILE* fp;
-if (argc < 2) 
+if (argc < 3) 
 	{
-	printf("Incorrect no. of input, check you entered file name \n");
+	printf("Incorrect no. of input, check you entered file name to compress and output file \n");
 	return(1);
         }
 infile =argv[1];
@@ -131,7 +115,7 @@ printf("After reading file we have ROWS and COLS: ");
 printf("%d, %d \n", ROWS, COLS);
 int**datat = transpose(data,ROWS, COLS); 
 printf("Done transposing \n");
-compressf(datat, ROWS, COLS, "compressed.txt");
+compressf(datat, ROWS, COLS, outfile);
 free(data);
 return(0);
 }
